7c27803f3f9767b1d856661d6db781922ea709e8
Bilusca <lucasav2005@hotmail.com>
2017-08-27 23:36:06 -0300

    Add Coverage

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -14,9 +14,13 @@ describe('Main', () => {
 
     it('should return `FizzBuzz` when multiple of 3 and multiple of 5', () => {
         expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
-    })
+    });
 
     it('should return Num when non-mutiple', () => {
         expect(FizzBuzz(7)).to.be.equal(7);
-    })
+    });
+
+    it('should return 0 when 0', () => {
+        expect(FizzBuzz(0)).to.be.equal(0);
+    });
 });
